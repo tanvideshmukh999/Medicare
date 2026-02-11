47d862cf2ab3bb8ed849ec1c5a620364779c9a0e
Tanvi <tanvideshmukh999@gmail.com>
2026-02-11 12:51:19 +0530

    Admin navbar added

---
+++ admin/src/pages/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import { heroStyles } from '../assets/dummyStyles';
+
+const Hero = () => {
+  return (
+    <div className={heroStyles.container}>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Hero
